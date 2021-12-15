67d622cc3939cde2e913f2524f8f7a38e3e0bbb7
YihuiTham <yihuitham@gmail.com>
2021-12-15 14:39:25 +0800

    set up Patient schema

---
+++ models/patient.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const patientSchema = mongoose.Schema({
+  name: { type: String, require: true },
+  id: { type: Number, require: true },
+  gender: { type: String, require: true },
+  age: { type: Number, require: true },
+  bloodType: { type: String, require: true },
+  medicalConditions: { type: String, require: true },
+  medicalRecords: { type: String },
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
