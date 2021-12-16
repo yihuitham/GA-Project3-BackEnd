6fdb6d77c002ef51efff3d05077ec48210195bd1
YihuiTham <yihuitham@gmail.com>
2021-12-16 22:15:23 +0800

    test mongoose-sequence

--- models/patient.js
+++ models/patient.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const patientSchema = mongoose.Schema({
   name: { type: String, require: true },
-  id: { type: Number, require: true, min: 7 },
+  // id: { type: Number, require: true, min: 7, unique: true },
   gender: { type: String, require: true },
   age: { type: Number, require: true },
   bloodType: { type: String, require: true },
   medicalConditions: { type: String, require: true },
   medicalRecords: { type: String },
 });
+
+patientSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
 module.exports = mongoose.model('Patient', patientSchema);
