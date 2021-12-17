32853bc681152c07172fc6aaed1674e718e066e9
YihuiTham <yihuitham@gmail.com>
2021-12-17 15:31:07 +0800

    edit operation model to link to staff model

--- models/operation.js
+++ models/operation.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const operationSchema = mongoose.Schema({
   operatingRoom: { type: Number, require: true, min: 1, max: 8 },
-  surgeonID: { type: String, require: true },
-  nursesID: [{ type: String, require: true }],
+  surgeonID: { type: mongoose.Types.ObjectId, required: true, ref: 'Staff' },
+  nursesID: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Staff' }],
   patientID: { type: String, require: true },
   date: { type: String, require: true },
   time: { type: String, require: true },
   description: { type: String, require: true },
 });
+
+operationSchema.plugin(AutoIncrement, { inc_field: 'operation_id' });
 
 module.exports = mongoose.model('Operation', operationSchema);
