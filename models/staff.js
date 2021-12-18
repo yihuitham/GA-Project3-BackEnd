d62e6ace6f4b39391bd21aa97050ddc3150fa2d8
kienwei96 <kien_wei@hotmail.com>
2021-12-18 16:32:38 +0800

    update staff schema model

--- models/staff.js
+++ models/staff.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const staffSchema = mongoose.Schema({
+  loginID: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  NRIC: { type: String, require: true, unique: true },
   name: { type: String, require: true },
   gender: { type: String, require: true },
   contact: { type: Number, require: true },
