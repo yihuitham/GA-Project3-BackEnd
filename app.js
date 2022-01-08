133a475b827ddbd9de4e4ad576aef07ba09751cb
kienwei96 <kien_wei@hotmail.com>
2022-01-08 11:35:01 +0800

    moved attachUser to app.js

--- app.js
+++ app.js
@@ -31,8 +31,29 @@ app.use(express.static('public'));
 
 //CONTROLLERS
 // login controller
-const loginController = require('./controllers/authenicate');
+const loginController = require('./controllers/authenticate');
 app.use('/api/authenticate', loginController);
+
+// attach user to the Request object/api
+const attachUser = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication invalid' });
+  }
+
+  const decodedToken = jwtDecode(token.slice(7));
+
+  if (!decodedToken) {
+    return res
+      .status(401)
+      .json({ message: 'There was a problem authorizing the request' });
+  } else {
+    req.user = decodedToken;
+    next();
+  }
+};
+
+app.use(attachUser);
 
 //patient controller
 const patientController = require('./controllers/patient');
