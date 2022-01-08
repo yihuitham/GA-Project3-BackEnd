04d6462c392b312ed8d3c3d920332a490faf6996
kienwei96 <kien_wei@hotmail.com>
2022-01-08 11:33:44 +0800

    re-named the file

---
+++ controllers/authenticate.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const jwtDecode = require('jwt-decode');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Staff = require('../models/staff');
+
+const { createToken, hashPassword, verifyPassword } = require('../util');
+
+// hi boss, these middlewares are alr injected at the app.js level, where all routes would be able to use
+// router.use(cors());
+// router.use(bodyParser.urlencoded({ extended: false }));
+// router.use(bodyParser.json());
+
+router.post('/', async (req, res) => {
+  try {
+    const { loginID, password } = req.body;
+
+    const user = await Staff.findOne({
+      loginID,
+    }).lean();
+
+    if (!user) {
+      return res.status(403).json({
+        message: 'Wrong email or password.',
+      });
+    }
+
+    const passwordValid = await verifyPassword(password, user.password);
+
+    if (passwordValid) {
+      const { password, ...rest } = user;
+      const userInfo = Object.assign({}, { ...rest });
+
+      const token = createToken(userInfo);
+
+      const decodedToken = jwtDecode(token);
+      const expiresAt = decodedToken.exp;
+
+      res.json({
+        message: 'Authentication successful!',
+        token,
+        userInfo,
+        expiresAt,
+      });
+    } else {
+      res.status(403).json({
+        message: 'Wrong email or password.',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: 'Something went wrong.' });
+  }
+});
+
+module.exports = router;
